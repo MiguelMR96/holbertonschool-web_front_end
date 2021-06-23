d669630c600b405a3a67fe4f238934ee79c16022
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-06-22 23:05:38 -0500

    Task 7

---
+++ 0x0C-Javascript_advanced/7-timeout.js
@@ -0,0 +1,8 @@
+console.log('Start of the execution queue');
+setTimeout(function() {
+  console.log('Final code block to be executed');
+  }, 0)
+for (let index = 1; index < 101; index++) {
+  console.log(index);
+}
+console.log('End of the loop printing');
