1f336da15790fac4b5343b9ddd6993688ddf611a
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-06-29 16:22:54 -0500

    Updating task 9 and 10

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,10 @@
+function countPrimeNumbers() {
+  let primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+  return primeNumbers.length;
+}
+
+const t0 = performance.now();
+countPrimeNumbers();
+const t1 = performance.now();
+console.log(`Execution time of printing countPrimeNumbers was ${t1 - t0} milliseconds.`);
